a79417ffc1ebe0d3dfe3c9ad566dbb4910280d9e
klopetx <klopetx@protonmail.com>
2021-05-22 12:31:47 +0200

    Calling scrapping functions.

--- SCRAP/python_scap.py
+++ SCRAP/python_scap.py
@@ -4,12 +4,19 @@ import requests
 import urllib.request
 import time
 from bs4 import BeautifulSoup
+from aux_scraping import eskuratu_doinuen_metadatuak
+import numpy as np
+
 
 import os
 #%%
 
-doinutegia_page = 'https://bdb.bertsozale.eus/web/doinutegia/emaitzak?bilatu=&izena=&hidden_izena=&mota=0&sortzailea=&hidden_sortzailea=&bertsolaria=&hidden_bertsolaria=&jasotzailea=&hidden_jasotzailea=&jasoa=&hidden_jasoa=&urtea=&kriterioak_gorde=1'
 
+
+#%%
+
+
+master_hiztegia = eskuratu_doinuen_metadatuak()
 # Final number represents the first record to start showing from, there are currently 3162 doinu
 
 #https://bdb.bertsozale.eus/web/doinutegia/emaitzak?bilatu=&izena=&hidden_izena=&mota=0&sortzailea=&hidden_sortzailea=&bertsolaria=&hidden_bertsolaria=&jasotzailea=&hidden_jasotzailea=&jasoa=&hidden_jasoa=&urtea=&kriterioak_gorde=1&per_page=3150
@@ -26,29 +33,52 @@ soup = BeautifulSoup(response.text, "html.parser")
 
 audio_files = soup.findAll('audio')
 
+#%%
+
+page_headers = soup.findAll('h4')
+
+h4 = page_headers
+# for each h4 
+
+
 
 #%%
 
-def sortu_patha(doinuaren_urla):
-    import re
+doinu_link = h4[1].find('a')
+doinu_link  = doinu_link.get('href')
 
-    current_dir = os.getcwd()
+doinu_link
 
-    audio_ref_zenbakia = re.search( 'mp3a/[0-9]*', doinuaren_urla)
+doinu_response = requests.get(doinu_link)
 
-    audio_ref_zenbakia = audio_ref_zenbakia.group(0)
+doinu_parsed = BeautifulSoup(doinu_response.text, "html.parser")
 
-    audio_ref_zenbakia = re.sub( pattern = 'mp3a/',string = audio_ref_zenbakia, repl = '')
+doinu_parsed.findAll('td')
 
-    return os.path.join(current_dir, audio_ref_zenbakia + '.mp3')
+#%%
 
 
-def deskargatu_doinua(audio_file):
 
-    doinuaren_urla = audio_file['src']
-    urllib.request.urlretrieve( doinuaren_urla, sortu_patha(doinuaren_urla) )
 
 #%% 
+
+#def scrap_bertso_web
+
+
+# inside td find the one with title "Midia eskuratu"
+#%% 
+
+
+#eskuratu_soinu_urlak(doinu_parsed)
+_eskuratu_soinu_urlak('')
+
+#%%
+
+
+#%% 
+
+
+
 
 for file_n in range(len(audio_files)):
     print(file_n)
